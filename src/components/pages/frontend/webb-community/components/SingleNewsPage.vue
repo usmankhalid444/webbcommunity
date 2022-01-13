9b76245c05e57fd2dd7113a2fd84b66aea5d5282
usman <usman.khalid444@gmail.com>
2022-01-14 00:23:07 +0500

    related news slider added

--- src/components/pages/frontend/webb-community/components/SingleNewsPage.vue
+++ src/components/pages/frontend/webb-community/components/SingleNewsPage.vue
@@ -33,23 +33,83 @@
         <h1 class="text-3xl md:text-5xl font-semibold inline">Related News</h1>
         <p class="float-right inline">
           <i
-            class="ri-arrow-left-s-line text-3xl px-2 text-custom-lightgray"
+            @click="prev_slide()"
+            class="ri-arrow-left-s-line text-3xl px-2 text-custom-lightgray hover:text-custom-darkblue cursor-pointer"
           ></i
-          ><i class="ri-arrow-right-s-line text-3xl px-2 "></i>
+          ><i
+            @click="next_slide()"
+            class="ri-arrow-right-s-line text-3xl px-2 text-custom-lightgray hover:text-custom-darkblue cursor-pointer"
+          ></i>
         </p>
       </div>
-      <div class="grid grid-cols-12 gap-5 md:gap-x-14">
+      <!-- <div class="grid grid-cols-12 gap-5 md:gap-x-14">
         <RelatedNewsCard />
         <RelatedNewsCard />
-      </div>
+      </div> -->
+      <section class="px-5">
+        <carousel
+          ref="news_slider"
+          :items-to-show="2.5"
+          snapAlign="start"
+          transition="1000"
+          :breakpoints="breakpoints"
+        >
+          <slide v-for="slide in 10" :key="slide">
+            <RelatedNewsCard />
+          </slide>
+        </carousel>
+      </section>
     </div>
   </FrontendMain>
 </template>
 <script>
+import "vue3-carousel/dist/carousel.css"; //https://ismail9k.github.io/vue3-carousel/
+import { Carousel, Slide } from "vue3-carousel";
 import RelatedNewsCard from "./RelatedNewsCard";
 export default {
   components: {
     RelatedNewsCard,
+    Carousel,
+    Slide,
+  },
+  data() {
+    return {
+      breakpoints: {
+        // 700px and up
+        0: {
+          itemsToShow: 1,
+          snapAlign: "center",
+        },
+        480: {
+          itemsToShow: 2,
+          snapAlign: "start",
+        },
+        // 1024 and up
+        1024: {
+          itemsToShow: 2.5,
+          snapAlign: "start",
+        },
+      },
+    };
+  },
+  methods: {
+    next_slide() {
+      this.$refs.news_slider.next();
+    },
+    prev_slide() {
+      this.$refs.news_slider.prev();
+    },
   },
 };
 </script>
+<style>
+.carousel__slide {
+  padding: 10px;
+}
+.carousel__prev,
+.carousel__next {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+</style>
